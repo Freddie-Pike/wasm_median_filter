28f51ef3083cf2e8e969fd1b8762bd1b666dcd22
Freddie Pike <Freddie@colabsoftware.com>
2019-07-01 11:28:40 -0230

    Cleaned up rust median filter code with loops. Added start script for http server.

--- img_test.rs
+++ img_test.rs
@@ -23,56 +23,28 @@ fn grey_scale( src: usize, dst : usize, w : usize, h :usize ) {
     
     for i in 0..w { 
         for j in 0..h { 
-            let pixel1 = src_img[((i - 1) * w) + (j - 1)];
-            let pixel2 = src_img[((i - 1) * w) + j];
-            let pixel3 = src_img[((i - 1) * w) + (j + 1)];
-            let pixel4 = src_img[(i * w) + (j - 1)];
-            let pixel5 = src_img[(i * w) + j];
-            let pixel6 = src_img[(i * w) + (j + 1)];
-            let pixel7 = src_img[((i + 1) * w) + (j - 1)];
-            let pixel8 = src_img[((i + 1) * w) + j];
-            let pixel9 = src_img[((i + 1) * w) + (j + 1)];
+            let mut kern: [u32; 9] = [0;9];
+            let mut off = 0;
+            for r in i-1..=i+1 {
+                for c in j-1..=j+1 {
+                    kern[off] = src_img[ r*w + c];
+                    off += 1;
+                }
+            }
 
-            let mut red_sum = 0;
-            red_sum += pixel1 & 0xff;
-            red_sum += pixel2 & 0xff;
-            red_sum += pixel3 & 0xff;
-            red_sum += pixel4 & 0xff;
-            red_sum += pixel5 & 0xff;
-            red_sum += pixel6 & 0xff;
-            red_sum += pixel7 & 0xff;
-            red_sum += pixel8 & 0xff;
-            red_sum += pixel9 & 0xff;
-            red_sum = red_sum / 9;
+            let mut rsum = 0;
+            let mut gsum = 0;
+            let mut bsum = 0;
+            for v in &kern {
+                rsum += v & 0xff;
+                gsum += (v >> 8 as u32) & 0xff;
+                bsum += (v >> 16 as u32) & 0xff;
+            }
 
-            let mut green_sum = 0;
-            green_sum += (pixel1 >> 8) & 0xff;
-            green_sum += (pixel2 >> 8) & 0xff;
-            green_sum += (pixel3 >> 8) & 0xff;
-            green_sum += (pixel4 >> 8) & 0xff;
-            green_sum += (pixel5 >> 8) & 0xff;
-            green_sum += (pixel6 >> 8) & 0xff;
-            green_sum += (pixel7 >> 8) & 0xff;
-            green_sum += (pixel8 >> 8) & 0xff;
-            green_sum += (pixel9 >> 8) & 0xff;
-            green_sum = green_sum / 9;
-
-            let mut blue_sum = 0;
-            blue_sum += (pixel1 >> 16) & 0xff;
-            blue_sum += (pixel2 >> 16) & 0xff;
-            blue_sum += (pixel3 >> 16) & 0xff;
-            blue_sum += (pixel4 >> 16) & 0xff;
-            blue_sum += (pixel5 >> 16) & 0xff;
-            blue_sum += (pixel6 >> 16) & 0xff;
-            blue_sum += (pixel7 >> 16) & 0xff;
-            blue_sum += (pixel8 >> 16) & 0xff;
-            blue_sum += (pixel9 >> 16) & 0xff;
-            blue_sum = blue_sum / 9;
-
-            let red = pixel1 & 0xff;
-            let green = (pixel1 >> 8) & 0xff;
-            let blue = (pixel1 >> 16) & 0xff;
-            dst_img[(i * w) + j] = red_sum | (green_sum<<8) | (blue_sum<<16) | (0xff<<24);
+            rsum = rsum / (kern.len() as u32);
+            gsum = gsum / (kern.len() as u32);
+            bsum = bsum / (kern.len() as u32);
+            dst_img[(i * w) + j] = rsum | (gsum<<8) | (bsum<<16) | (0xff<<24);
         }
     }
 }
